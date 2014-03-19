4feedc1458f241b4bbeaaf7ad6d7d09894606a62
Hugh Kennedy <hughskennedy@gmail.com>
2014-03-19 12:46:56 +1100

    Fixes for mixing into prototypes

--- index.js
+++ index.js
@@ -11,11 +11,12 @@ function PrototypeEmitter(Original) {
   function Base() {
     EventEmitter.call(this)
 
+    var onceEvents = Base.prototype._onceEvents
+
     for (var i = 0; i < onceEvents.length; i++) {
       this.once.apply(this, onceEvents[i])
     }
   }
-
 
   var proto = Base.prototype
   var _emit = Base.prototype.emit
@@ -27,35 +28,45 @@ function PrototypeEmitter(Original) {
   Base.removeListener = off
   Base.off = off
 
-  var onceEvents = proto._onceEvents = []
-  var sharedEvents = proto._sharedEvents = {}
+  proto._onceEvents = []
+  proto._sharedEvents = {}
 
   proto.emit = emit
 
   if (!Original) return Base
 
-  // Allow prototype-emitter to be applied
-  // as a class mixin.
-  inherits(Original, Base)
+  // Allow prototype-emitter to be applied as a class mixin.
+  // include Original's prototype
+  var copied = Object.create(Original.prototype)
+  Base.prototype = copied
 
-  Object.keys(Base).forEach(function(key) {
-    Original[key] = Base[key]
-  })
+  // include prototype-element's prototype
+  extend(Base.prototype, proto, false)
 
-  return Original
+  // include EventEmitter's prototype
+  extend(Base.prototype, EventEmitter.prototype, true)
+
+  // Pass static methods from Base onto Original class too
+  extend(Original, Base, false)
+
+  Original.prototype = Base.prototype
+
+  return Base = Original
 
   function on(name, fn) {
+    var sharedEvents = Base.prototype._sharedEvents
     sharedEvents[name] = sharedEvents[name] || []
-    sharedEvents[name].push(name, fn)
+    sharedEvents[name].push(fn)
     return Base
   }
 
   function once() {
-    onceEvents.push(slice(arguments))
+    Base.prototype._onceEvents.push(slice(arguments))
     return Base
   }
 
   function off(name, fn) {
+    var sharedEvents = Base.prototype._sharedEvents
     var events = sharedEvents[name]
     if (!events) return Base
     var idx = events.indexOf(fn)
@@ -69,9 +80,10 @@ function PrototypeEmitter(Original) {
   function emit(name, a, b, c, d) {
     if (!name) return
 
+    var sharedEvents = Base.prototype._sharedEvents
     var events = sharedEvents[name]
     var length = events && events.length
-    var i = 0
+    var i = -1
 
     if (length) {
       switch (arguments.length) {
@@ -84,10 +96,21 @@ function PrototypeEmitter(Original) {
       }
     }
 
-    if (this._events[name]) {
+    if (this._events && this._events[name]) {
       _emit.apply(this, slice(arguments))
     }
 
     return this
   }
+}
+
+function extend(a, b, respectful) {
+  Object.keys(b).forEach(function(k) {
+    if (k === 'super_') return
+    a[k] = respectful
+      ? a[k] || b[k]
+      : b[k]
+  })
+
+  return a
 }
